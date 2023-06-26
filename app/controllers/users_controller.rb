204022c82577869c36874e05b7ea6eb9ea4a10be
jolak5 <kayodej4@gmail.com>
2023-06-26 15:49:17 +0100

    set up the views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update destroy]
-
+  # before_action :set_user, only: %i[show edit update destroy]
+  before_action :authenticate_user!
   # GET /users or /users.json
   def index
     @users = User.all
@@ -65,5 +65,6 @@ class UsersController < ApplicationController
   # Only allow a list of trusted parameters through.
   def user_params
     params.require(:user).permit(:name)
+    p params
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "home#index"
+  root to: "foods#index"
   resources :recipe_foods
   resources :inventory_foods
   resources :foods
