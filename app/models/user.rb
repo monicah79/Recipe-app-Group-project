e8096fc1324c371eee72f5d8610e0981a33c2c8c
Damilare Adepoju <108327427+adamilare@users.noreply.github.com>
2023-06-26 14:18:17 +0100

    Fix merge conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ApplicationRecord
-<<<<<<< Updated upstream
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-=======
->>>>>>> Stashed changes
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
   devise_for :users
   root to: "home#index"
-=======
   resources :recipe_foods
   resources :inventory_foods
   resources :foods
   resources :recipes
   resources :inventories
   resources :users
->>>>>>> Stashed changes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
