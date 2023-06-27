ee54a6aa56e3928d2b3882c8543db55350abcd78
monicah79 <monniewanjema@gmail.com>
2023-06-28 02:40:49 +0300

    Fix linters

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,8 +1,8 @@
 class ShoppingListController < ApplicationController
-    def index
-        current_user
-        @recipes = current_user.recipes
-        @foods = @recipes.flat_map(&:foods).uniq
-        @total_price = @recipes.sum(&:total_price)
-      end
+  def index
+    current_user
+    @recipes = current_user.recipes
+    @foods = @recipes.flat_map(&:foods).uniq
+    @total_price = @recipes.sum(&:total_price)
+  end
 end
--- spec/requests/public_recipes_spec.rb
+++ spec/requests/public_recipes_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 require 'capybara/rails'
 
 RSpec.describe 'PublicRecipes', type: :request do
-    describe 'GET /index' do
-      it 'returns http success' do
-        get '/public_recipes'
-        expect(response).to have_http_status(:success)
-      end
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/public_recipes'
+      expect(response).to have_http_status(:success)
     end
   end
+end
