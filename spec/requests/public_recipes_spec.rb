17ef741adc5f2818d39c2bc4a70dab6beaf4f8ff
monicah79 <monniewanjema@gmail.com>
2023-06-28 02:26:56 +0300

    Spec test for public recipe

---
+++ spec/requests/public_recipes_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+require 'capybara/rails'
+
+RSpec.describe 'PublicRecipes', type: :request do
+    describe 'GET /index' do
+      it 'returns http success' do
+        get '/public_recipes'
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
