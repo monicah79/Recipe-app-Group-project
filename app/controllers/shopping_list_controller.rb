30b01f6d44e4ac7d31ec45d49c4105bf357f10f8
Damilare Adepoju <108327427+adamilare@users.noreply.github.com>
2023-06-28 18:35:11 +0100

    Temporary branch for shoppinglist

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -2,7 +2,26 @@ class ShoppingListController < ApplicationController
   def index
     current_user
     @recipes = current_user.recipes
-    @foods = @recipes.flat_map(&:foods).uniq
-    @total_price = @recipes.sum(&:total_price)
+    # @foods = @recipes.flat_map(&:foods).uniq
+    # @total_price = @recipes.sum(&:total_price)
+
+    # recipe_id = params[:recipe_id]
+    # inventory_id = params[:inventory_id]
+
+    @joined_table = @joined_table = RecipeFood
+      .select('recipe_foods.id, foods.name, foods.price, foods.measurement_unit, recipe_foods.quantity AS rc_qty, inventory_foods.quantity AS inv_qty, inventory_foods.id')
+      .joins(:food)
+      .left_joins(food: :inventory_foods)
+      .where(recipe_id: 1, 'inventory_foods.inventory_id': 1)
+
+    @joined_table.each do |record|
+      puts "Food Name: #{record.name}"
+      puts "Food Price: #{record.price}"
+      puts "Measurement Unit: #{record.measurement_unit}"
+      puts "Recipe Quantity: #{record.rc_qty}"
+      puts "Inventory Quantity: #{record.inv_qty}"
+      puts "Inventory Food ID: #{record.id}"
+      puts "---------------------"
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-first_user = User.create(name: 'Tom', email: "mail1@gmai.com", password: "123456", password_confirmation: "123456")
+
+first_user = User.create(name: 'Tom', email: "mail1@gmail.com", password: "123456", password_confirmation: "123456")
 second_user = User.create(name: 'Lilly', email: "mail2@gmail.com", password: "123456", password_confirmation: "123456")
 
 first_inventory = Inventory.create(user: first_user, name: 'Inventory 1', description: "Inventory1 description")
 second_inventory = Inventory.create(user: second_user, name: 'Inventory 2', description: "Inventory1 description")
 
-first_food = Food.create(name: "Food 1", price: "1000", measurement_unit: "gram")
-second_food = Food.create(name: "Food 2", price: "2000", measurement_unit: "gram")
+first_food = Food.create(name: "Food 1", price: "100", measurement_unit: "kg")
+second_food = Food.create(name: "Food 2", price: "200", measurement_unit: "kg")
+third_food = Food.create(name: "Food 3", price: "150", measurement_unit: "kg")
 
-first_inventory_food = InventoryFood.create(quantity: 20, inventory: first_inventory, food: first_food)
-second_inventory_food = InventoryFood.create(inventory: first_inventory, food: second_food)
-third_inventory_food = InventoryFood.create(inventory: second_inventory, food: second_food)
+first_inventory_food = InventoryFood.create(quantity: 2, inventory: first_inventory, food: first_food)
+second_inventory_food = InventoryFood.create(quantity: 2, inventory: first_inventory, food: second_food)
+third_inventory_food = InventoryFood.create(quantity: 4, inventory: second_inventory, food: second_food)
+
+first_recipe = Recipe.create(name: "Recipe 1", preparation_time: "2", cooking_time: "1", description: "Description for Recipe 1", public: true, user: first_user)
+second_recipe = Recipe.create(name: "Recipe 2", preparation_time: "2", cooking_time: "1", description: "Description for Recipe 2", public: true, user: first_user)
+third_recipe = Recipe.create(name: "Recipe 3", preparation_time: "2", cooking_time: "1", description: "Description for Recipe 3", public: false, user: first_user)
+
+first_recipe_food = RecipeFood.create(quantity: 3, recipe: first_recipe, food: first_food)
+second_recipe_food = RecipeFood.create(quantity: 2, recipe: first_recipe, food: second_food)
+third_recipe_food = RecipeFood.create(quantity: 3, recipe: first_recipe, food: third_food)
