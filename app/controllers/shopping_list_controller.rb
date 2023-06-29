ded58194197480ffd507ceb5025b5af1981a7220
Damilare Adepoju <108327427+adamilare@users.noreply.github.com>
2023-06-29 12:06:37 +0100

    Shopping list display its details

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -30,35 +30,51 @@ class ShoppingListController < ApplicationController
     current_user
     @recipe = Recipe.find(1)
 
-    @recipe_list = @joined_table = RecipeFood
-      .select('recipe_foods.id, foods.name, foods.price, foods.measurement_unit, recipe_foods.quantity AS rc_qty, inventory_foods.quantity AS inv_qty, inventory_foods.id')
-      .joins(:food)
-      .left_joins(food: :inventory_foods)
-      .where(recipe_id: 1, 'inventory_foods.inventory_id': 1)
+    p '---------', params, '------------', request, '-------------'
 
-    @shopping_list = []
+    @recipe_id = params[:recipe_id]
+    @inventory_id = params[:inventory_id]
+    
 
-    @recipe_list.each do |record|
-      puts "Food Name: #{record.name}"
-      puts "Food Price: #{record.price}"
-      puts "Measurement Unit: #{record.measurement_unit}"
-      puts "Recipe Quantity: #{record.rc_qty}"
-      puts "Inventory Quantity: #{record.inv_qty}"
-      puts "Inventory Food ID: #{record.id}"
-      puts "---------------------"
+    if(@recipe_id && @inventory_id)
+      @recipe = RecipeFood.find(@recipe_id).recipe
+      @inventory = InventoryFood.find(@inventory_id).inventory
 
-      if record.inv_qty < record.rc_qty
-        shopping_qty = record.rc_qty - record.inv_qty
-        shopping_amount = shopping_qty * record.price.to_i
+      @recipe_list = @joined_table = RecipeFood
+        .select('recipe_foods.id, foods.name, foods.price, foods.measurement_unit, recipe_foods.quantity AS rc_qty, inventory_foods.quantity AS inv_qty, inventory_foods.id')
+        .joins(:food)
+        .left_joins(food: :inventory_foods)
+        .where(recipe_id: 1, 'inventory_foods.inventory_id': 1)
+  
+      @shopping_list = []
+      @total_amount = 0
 
-        @shopping_list << {
-          name: record.name,
-          price: record.price,
-          measurement_unit: record.measurement_unit,
-          qty: shopping_qty,
-          amount: shopping_amount
-        }      
+      @recipe_list.each do |record|
+        puts "Food Name: #{record.name}"
+        puts "Food Price: #{record.price}"
+        puts "Measurement Unit: #{record.measurement_unit}"
+        puts "Recipe Quantity: #{record.rc_qty}"
+        puts "Inventory Quantity: #{record.inv_qty}"
+        puts "Inventory Food ID: #{record.id}"
+        puts "---------------------"
+  
+        if record.inv_qty < record.rc_qty
+          shopping_qty = record.rc_qty - record.inv_qty
+          shopping_amount = shopping_qty * record.price.to_i
+          
+          @total_amount += shopping_amount
+
+          @shopping_list << {
+            name: record.name,
+            price: record.price,
+            measurement_unit: record.measurement_unit,
+            qty: shopping_qty,
+            amount: shopping_amount
+          }      
+        end
       end
+    else
+      flash.now[:notice] = "Please make sure you have selected valid Inventory and Recipe"
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,10 @@ Rails.application.routes.draw do
     patch 'toggle_visibility', on: :member
   end
   resources :public_recipes
-  resources :shopping_list
+  # resources :shopping_list
   resources :inventories
   resources :users
+  get '/shopping_list', to: 'shopping_list#show', as: 'shopping_list'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
