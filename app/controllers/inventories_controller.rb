bcaf581dd0c1b72f2812cd20af69bbf4acc1014c
Damilare Adepoju <108327427+adamilare@users.noreply.github.com>
2023-06-29 16:59:38 +0100

    Fix linter error

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -62,7 +62,7 @@ class InventoriesController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_inventory
-     @inventory = current_user.inventories.includes(inventory_foods: :food).find(params[:id])
+    @inventory = current_user.inventories.includes(inventory_foods: :food).find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,6 +1,6 @@
 class ShoppingListController < ApplicationController
   before_action :authenticate_user!
-  
+
   def show
     current_user
     @recipe = Recipe.find(1)
@@ -10,53 +10,53 @@ class ShoppingListController < ApplicationController
     @shopping_list = []
     @total_amount = 0
 
-    if(@recipe_id && @inventory_id)
+    if @recipe_id && @inventory_id
       @recipe = RecipeFood.find(@recipe_id).recipe
       @inventory = InventoryFood.find(@inventory_id).inventory
 
       @recipe_list = RecipeFood
-        .select('recipe_foods.id, foods.name, foods.price, foods.measurement_unit, recipe_foods.quantity AS rc_qty, inventory_foods.quantity AS inv_qty, inventory_foods.id')
+        .select('recipe_foods.id, foods.name, foods.price, foods.measurement_unit,
+          recipe_foods.quantity AS rc_qty, inventory_foods.quantity AS inv_qty, inventory_foods.id')
         .joins(:food)
-        .joins("LEFT JOIN inventory_foods ON foods.id = inventory_foods.food_id AND inventory_foods.inventory_id = 1")
+        .joins('LEFT JOIN inventory_foods ON foods.id = inventory_foods.food_id AND inventory_foods.inventory_id = 1')
         .where(recipe_id: @recipe_id)
-      
+
       process_recipe_list
 
     else
-      flash.now[:notice] = "Please make sure you have selected valid Inventory and Recipe"
+      flash.now[:notice] = 'Please make sure you have selected valid Inventory and Recipe'
     end
   end
 
-  def print_recod record
+  def print_recod(record)
     puts "Food Name: #{record.name}"
     puts "Food Price: #{record.price}"
     puts "Measurement Unit: #{record.measurement_unit}"
     puts "Recipe Quantity: #{record.rc_qty}"
     puts "Inventory Quantity: #{record.inv_qty}"
     puts "Inventory Food ID: #{record.id}"
-    puts "---------------------"
+    puts '---------------------'
   end
 
   def process_recipe_list
-    @recipe_list.each do |record|    
-      print_recod(record)    
-      record.inv_qty = 0 unless record.inv_qty != nil
-        
-      if record.inv_qty < record.rc_qty
-        
-        shopping_qty = record.rc_qty - record.inv_qty
-        shopping_amount = shopping_qty * record.price.to_i
-        
-        @total_amount += shopping_amount
+    @recipe_list.each do |record|
+      print_recod(record)
+      record.inv_qty = 0 if record.inv_qty.nil?
 
-        @shopping_list << {
-          name: record.name,
-          price: record.price,
-          measurement_unit: record.measurement_unit,
-          qty: shopping_qty,
-          amount: shopping_amount
-        }      
-      end
+      next unless record.inv_qty < record.rc_qty
+
+      shopping_qty = record.rc_qty - record.inv_qty
+      shopping_amount = shopping_qty * record.price.to_i
+
+      @total_amount += shopping_amount
+
+      @shopping_list << {
+        name: record.name,
+        price: record.price,
+        measurement_unit: record.measurement_unit,
+        qty: shopping_qty,
+        amount: shopping_amount
+      }
     end
   end
 end
