61c22525d90ff5c2041724577c048c6b25bb035b
jolak5 <kayodej4@gmail.com>
2023-07-01 16:03:10 +0100

    fix linters

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,7 +1,5 @@
 class PublicRecipesController < ApplicationController
   def index
-    
     @public_recipes = Recipe.includes(:user, :food, :recipe_foods).where(public: true).order(created_at: :desc)
-    
- 
+  end
 end
