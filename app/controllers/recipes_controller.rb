b5977328c8abf324da59d8c495565dfececc066a
jolak5 <kayodej4@gmail.com>
2023-07-01 14:58:39 +0100

    Style allm modals and fix current users

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,15 +3,15 @@ class RecipesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_recipe, only: %i[show edit update destroy]
 
-  # GET /recipes or /recipes.json
+
   def index
-    @recipes = Recipe.all
+    @recipes = current_user.recipes
   end
 
-  # GET /recipes/1 or /recipes/1.json
+
   def show
     @recipe = Recipe.find(params[:id])
-    @inventory_id = params[:inventory_id] # Assuming inventory_id is passed as a parameter
+    @inventory_id = params[:inventory_id] 
 
     @inventories = Inventory.all
 
@@ -24,7 +24,6 @@ class RecipesController < ApplicationController
     @recipe_foods = RecipeFood.find_by(recipe_id: @recipe.id)
   end
 
-  # GET /recipes/new
   def new
     @recipe = Recipe.new
   end
@@ -34,16 +33,14 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:generate_shopping_list][:recipe_id])
     @inventory = Inventory.find(params[:generate_shopping_list][:inventory_id])
 
-    # Logic to generate shopping list using the selected inventory and recipe
 
     redirect_to shopping_list_path(recipe_id: @recipe.id, inventory_id: @inventory.id),
                 notice: 'Shopping list generated successfully.'
   end
 
-  # GET /recipes/1/edit
+
   def edit; end
 
-  # POST /recipies or /recipies.json
   def create
     current_user
     @recipe = current_user.recipes.build(recipe_params)
@@ -59,22 +56,6 @@ class RecipesController < ApplicationController
     end
   end
 
-  # # POST /recipes or /recipes.json
-  # def create
-  #   @recipe = Recipe.new(recipe_params)
-
-  #   respond_to do |format|
-  #     if @recipe.save
-  #       format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully created.' }
-  #       format.json { render :show, status: :created, location: @recipe }
-  #     else
-  #       format.html { render :new, status: :unprocessable_entity }
-  #       format.json { render json: @recipe.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # PATCH/PUT /recipes/1 or /recipes/1.json
   def update
     respond_to do |format|
       if @recipe.update(recipe_params)
@@ -87,7 +68,7 @@ class RecipesController < ApplicationController
     end
   end
 
-  # DELETE /recipes/1 or /recipes/1.json
+
   def destroy
     @recipe.destroy
 
@@ -105,12 +86,12 @@ class RecipesController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
+
   def set_recipe
     @recipe = Recipe.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
+
   def recipe_params
     params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public, :user_id)
   end
