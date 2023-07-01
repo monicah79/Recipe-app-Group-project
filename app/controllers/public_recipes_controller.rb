4e61b19e40a5f4862178cc72c48f0c6694349480
jolak5 <kayodej4@gmail.com>
2023-07-01 16:00:32 +0100

    Public recipe sorted

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,13 +1,7 @@
 class PublicRecipesController < ApplicationController
   def index
-    # @recipes = Recipe.public_recipes
-    @recipes = Recipe
-      .select('recipes.name AS recipe_name, users.name AS author_name
-      , COUNT(foods.id) AS food_count
-      , SUM(CAST(foods.price AS numeric) * recipe_foods.quantity) AS total_amount')
-      .joins(:user)
-      .joins(recipe_foods: :food)
-      .where(public: true)
-      .group('recipes.id, users.id')
-  end
+    
+    @public_recipes = Recipe.includes(:user, :food, :recipe_foods).where(public: true).order(created_at: :desc)
+    
+ 
 end
